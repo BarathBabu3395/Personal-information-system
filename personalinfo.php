6ebc2d82a587d87d03ddaf26df78aeb40f9952e0
BarathBabu3395 <112332937+BarathBabu3395@users.noreply.github.com>
2023-05-16 12:06:16 +0530

    Add files via upload

---
+++ personalinfo.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daily Routine</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 2px solid #ddd;
+        }
+        
+        tr:hover {background-color: #f5f5f5;}
+    </style>
+</head>
+<body bgcolor="pink">
+    <h1>Bio Data</h1>
+    <p>Name: Barath B</p>
+    <p>Age: 18</p>
+    <p>Occupation: IT Student</p>
+    
+    <h2>Daily Routine</h2>
+    <table>
+        <tr>
+            <th>Day</th>
+            <th>Time</th>
+            <th>Activity</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            <td>Monday</td>
+            <td>8:00 AM</td>
+            <td>Morning Exercise</td>
+            <td>Start the day with a workout.</td>
+        </tr>
+        <tr>
+            <td>Tuesday</td>
+            <td>9:00 AM</td>
+            <td>Breakfast</td>
+            <td>Have a healthy breakfast.</td>
+        </tr>
+        <tr>
+            <td>Wednesday</td>
+            <td>10:00 AM</td>
+            <td>Work</td>
+            <td>Start working on important tasks.</td>
+        </tr>
+        <tr>
+            <td>Thursday</td>
+            <td>1:00 PM</td>
+            <td>Lunch</td>
+            <td>Take a break and have lunch.</td>
+        </tr>
+        <tr>
+            <td>Friday</td>
+            <td>2:00 PM</td>
+            <td>Meetings</td>
+            <td>Attend scheduled meetings.</td>
+        </tr>
+        <tr>
+            <td>Saturday</td>
+            <td>4:00 PM</td>
+            <td>Snack</td>
+            <td>Grab a quick snack.</td>
+        </tr>
+        <tr>
+            <td>Sunday</td>
+            <td>6:00 PM</td>
+            <td>Exercise</td>
+            <td>Engage in physical activity.</td>
+        </tr>
+    </table>
+</body>
+</html>
